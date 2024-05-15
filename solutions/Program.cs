eb4a08767a5fb1059d6e2434555fa3898dc718bf
João Kennedy <joao.kennedy707@gmail.com>
2024-05-15 15:59:22 -0300

    1

---
+++ solutions/Program.cs
@@ -0,0 +1,15 @@
+﻿using solutions;
+using System;
+using System.Runtime.ConstrainedExecution;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        /*exercise01 exercise01 = new exercise01();
+        exercise01.Exercicio01();*/
+
+
+    }
+}
